82bf048bb94b9ab1e03d05d9944c90544cd7db53
EngJao89 <eng_joao@outlook.com>
2023-09-06 22:39:42 -0300

    feat(it13-table-activities): implementa histórico com tabela de atividades

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,30 @@
+import { HistoryContainer, HistoryList } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>History</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Duração</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Testes unitários</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
